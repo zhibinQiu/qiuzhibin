37d4895a68e0e8a318dd28098799075691fde755
qiuzhibin <2206411193@qq.com>
2018-10-28 12:22:30 +0800

    修正2

--- src/main/java/com/mall/service/IProductService.java
+++ src/main/java/com/mall/service/IProductService.java
@@ -1,22 +1,21 @@
 package com.mall.service;
 
 import com.github.pagehelper.PageInfo;
-<<<<<<< HEAD
-=======
+
 import com.mall.common.ServerResponse;
 import com.mall.pojo.Product;
 import com.mall.vo.ProductDetailVo;
->>>>>>> 8360355204d135e73030ee9d1a45deb156048445
+
 
 public interface IProductService {
 
     ServerResponse saveOrUpdateProduct(Product product);
 
-<<<<<<< HEAD
+
     ServerResponse<String> setSaleStatus(Integer productId,Integer status);
-=======
-    ServerResponse<String>setSaleStatus(Integer productId,Integer status);
->>>>>>> 8360355204d135e73030ee9d1a45deb156048445
+
+
+
 
     ServerResponse<ProductDetailVo> manageProductDetail(Integer productId);
 
@@ -27,9 +26,5 @@ public interface IProductService {
     ServerResponse<ProductDetailVo> getProductDetail(Integer productId);
 
     ServerResponse<PageInfo> getProductByKeywordCategory(String keyword,Integer categoryId,int pageNum,int pageSize,String orderBy);
-<<<<<<< HEAD
-=======
 
-
->>>>>>> 8360355204d135e73030ee9d1a45deb156048445
 }
